66c6e333e8e47f25da0e94887884319ba96a5649
Michael Peterson <WebDev@ctoms.ca>
2019-02-07 14:26:27 -0700

    added event initialization

--- js/bundle.js
+++ js/bundle.js
@@ -1,10 +1,11 @@
 function kitBuilder(containerID,buttonIDs){
 	this.kitContainer = document.getElementById(containerID);
 	this.bundleButtons = this.getButtons(buttonIDs);
+	this.bundleButtons.forEach(button => this.initButtons(button));
 }
 
 kitBuilder.prototype.getButtons = function(buttonIDs) {
-	var buttonArr = []
+	var buttonArr = [];
 	buttonIDs.forEach(buttonId => {
 		buttonArr.push(document.getElementById(buttonId));
 	});
@@ -12,10 +13,21 @@ kitBuilder.prototype.getButtons = function(buttonIDs) {
 	return buttonArr;
 }
 
+kitBuilder.prototype.initButtons = function(button){
+	console.log("test");
+	button.addEventListener("click",function(e){
+		this.buttonClicked(e)
+	}.bind(this),false);
+}
+
+kitBuilder.prototype.buttonClicked = function(event){
+	event.stopPropagation();
+	event.preventDefault();
+	console.log("clicked", event.target);
+}
+
 document.addEventListener( "DOMContentLoaded", function() {
 	var kit1 = new kitBuilder("bundle-container1",["bundle-button1","bundle-button2","bundle-button3"]);
 });
-
-
 
 console.log("test");
