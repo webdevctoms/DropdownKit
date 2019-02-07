f6528e13e62777203663d1ddc6980c66e2a6f8fc
Michael Peterson <WebDev@ctoms.ca>
2019-02-07 15:29:43 -0700

    added section expand functionality

--- js/bundle.js
+++ js/bundle.js
@@ -1,7 +1,20 @@
 function kitBuilder(containerID,buttonIDs){
 	this.kitContainer = document.getElementById(containerID);
 	this.bundleButtons = this.getButtons(buttonIDs);
+	this.bundleHeights = this.getHeights();
 	this.bundleButtons.forEach(button => this.initButtons(button));
+}
+
+kitBuilder.prototype.getHeights = function(){
+	var heights = [];
+	var bundleContentElements = document.getElementsByClassName("bundle-selector-content");
+	for(var i = 0;i < bundleContentElements.length;i++){
+		heights.push(bundleContentElements[i].clientHeight);
+		bundleContentElements[i].style.height = "0px";
+		//bundleContentElements[i].style.display = "none";
+	}
+
+	return heights;
 }
 
 kitBuilder.prototype.getButtons = function(buttonIDs) {
@@ -23,6 +36,11 @@ kitBuilder.prototype.buttonClicked = function(event){
 	event.stopPropagation();
 	event.preventDefault();
 	console.log("clicked", event.currentTarget);
+	console.log("next sibling", event.currentTarget.nextElementSibling);
+	var optionContent = event.currentTarget.nextElementSibling;	
+	var bundleId = event.currentTarget.dataset.bundleid;
+	console.log(this.bundleHeights, bundleId);
+	optionContent.firstElementChild.style.height = this.bundleHeights[bundleId] + "px";
 }
 
 document.addEventListener( "DOMContentLoaded", function() {
