b6e9d2c435553c05fb309da8a47183f011d2ba19
Michael Peterson <WebDev@ctoms.ca>
2019-02-07 13:58:15 -0700

    initial setup of kit object

--- js/bundle.js
+++ js/bundle.js
@@ -1,1 +1,21 @@
+function kitBuilder(containerID,buttonIDs){
+	this.kitContainer = document.getElementById(containerID);
+	this.bundleButtons = this.getButtons(buttonIDs);
+}
+
+kitBuilder.prototype.getButtons = function(buttonIDs) {
+	var buttonArr = []
+	buttonIDs.forEach(buttonId => {
+		buttonArr.push(document.getElementById(buttonId));
+	});
+	console.log(buttonArr);
+	return buttonArr;
+}
+
+document.addEventListener( "DOMContentLoaded", function() {
+	var kit1 = new kitBuilder("bundle-container1",["bundle-button1","bundle-button2","bundle-button3"]);
+});
+
+
+
 console.log("test");
